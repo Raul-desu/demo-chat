bb17aaa6bb2e7f7ffe97d56404e0529159ffadb1
Raul-desu <116002998+Raul-desu@users.noreply.github.com>
2022-10-17 22:51:01 +0800

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -656,9 +656,9 @@ class ChatBot(Client):
 cookies = {
     "sb": "xasyYmAoy1tRpMGYvLxgkHBF",
     "fr": "0NxayJuewRHQ30OX3.AWVJwIYNh0Tt8AJv6kSwDamhkoM.BiMrVd.Iu.AAA.0.0.BiMtVZ.AWXMVaiHrpQ",
-    "c_user": "",
+    "c_user": "100011495016677",
     "datr": "xasyYs51GC0Lq5H5lvXTl5zA",
-    "xs": ""
+    "xs": "17%3ASNjAN9BEb9HWIQ%3A2%3A1666014622%3A-1%3A8125"
 }
 
 
